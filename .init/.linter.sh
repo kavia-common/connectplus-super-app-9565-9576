278cb2619c86b6ce13681b8421653eea335c8700
Kavia AI <production@kavia.ai>
2026-01-29 06:14:17 +0000

    Checkpoint 1 - cg35851b3d

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/connectplus-super-app-9565-9576/cross_platform_frontend
+flutter analyze
+
---
+++ cross_platform_frontend/android/app/src/main/kotlin/com/example/cross_platform_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cross_platform_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
